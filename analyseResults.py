8cafefded0792cf5f7702936dfb92dcf0452ca07
mattlerner <m.a.lerner@gmail.com>
2016-06-28 23:10:55 -0400

    analyseResults.py working

--- analyseResults.py
+++ analyseResults.py
@@ -3,6 +3,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import sys
 
+arrayLine = {"sell":"r","buy":"g"}
+
 # read and print pickle
 df = pandas.read_pickle("results.pkl")
 print df.to_string()
@@ -11,13 +13,15 @@ print df.to_string()
 startingRow = sys.argv[1]
 endingRow = sys.argv[2]
 
-j = df[100:200]
+j = df[int(startingRow):int(endingRow)]
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.plot(j['price'])
 ax.plot(j['upperBand'])
 ax.plot(j['lowerBand'])
+ax.plot(j['movingAverage'])
+
+for index, row in j.iterrows():
+	if row['orderType'] is not '':
+		plt.axvline(x=index, linewidth=1, color=arrayLine[row['orderType']])
 plt.show()
-
-
-#plt.axvline(x=i, linewidth=1, color=self.arrayLine[side])
